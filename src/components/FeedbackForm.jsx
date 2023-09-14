e381dd239201fd09b20d871b61ea27970e32cbb4
Tanya Shylock <tourtanya@gmail.com>
2023-09-14 15:34:47 -0600

    bug fixes in form: reset form after submission

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -21,18 +21,19 @@ function FeedbackForm() {
 
     }, [ feedbackEdit ])
 
-  const handleTextChange = (e) => {
-    if (text === '') {
+  const handleTextChange = ({ target: { value } }) => {
+    if (value === '') {
       setBtnDisabled(true)
       setMessage(null)
-    } else if (text !== '' && text.trim().length < 10) {
+
+    } else if (value.trim().length < 10) { 
+      setMessage('Text must be at least 10 characters')
       setBtnDisabled(true)
-      setMessage('Message must be at least 10 characters')
     } else {
       setMessage(null)
       setBtnDisabled(false)
     }
-    setText(e.target.value)
+    setText(value)
   }
 
   const handleSubmit = (e) => {
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,45 +1,68 @@
-import { createContext, useState } from "react";
-import feedbackData from "../data/FeedbackData.js";
+import { createContext, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
-const defaultEditState = {
-  item: {},
-  edit: false,
-};
-
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState(feedbackData);
-  const [feedbackEdit, setFeedbackEdit] = useState(defaultEditState);
+  const [ feedback, setFeedback ] = useState([
+    {
+      id: 1,
+      text: 'This item is feedback item 1',
+      rating: 10
+    },
+    {
+      id: 2,
+      text: 'This item is feedback item 2',
+      rating: 7
+    },
+    {
+      id: 3,
+      text: 'This item is feedback item 3',
+      rating: 9
+    },
+  ])
 
-  // Add feedback
+  const [ feedbackEdit, setFeedbackEdit ] = useState({
+    item: {},
+    edit: false
+  })
+
+  // Add feedback item
   const addFeedback = (newFeedback) => {
-    setFeedback([newFeedback, ...feedback]);
-  };
+    newFeedback.id = parseInt(uuidv4())
+    setFeedback([newFeedback, ...feedback])
+  }
 
-  // Delete feedback
+  // Delete feedback item
   const deleteFeedback = (id) => {
-    if (window.confirm("Are you sure you want to delete?")) {
-      setFeedback(feedback.filter((item) => item.id !== id));
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    };
+  }
+
+  const defaultEditState = {
+    item: {},
+    edit: false,
     }
-  };
 
-  // Update feedback item
-  const updateFeedback = (id, updItem) => {
-    setFeedback(
-      feedback.map((item) => (item.id === id ? { ...updItem, id } : item))
-    );
-
-    setFeedbackEdit(defaultEditState);
-  };
-
-  // Set item to be updated
+  // Set feedback item to be updated
   const editFeedback = (item) => {
     setFeedbackEdit({
       item,
-      edit: true,
-    });
-  };
+      edit: true
+    })
+  }
+
+  // Update feedback item
+  const updateFeedback = (id, updatedItem) => {
+    setFeedback(
+      feedback.map((item) =>
+      (item.id === id
+      ? { ...item, ...updatedItem }
+      : item))
+    )
+    setFeedbackEdit(defaultEditState)
+  }
 
   return (
     <FeedbackContext.Provider
@@ -51,10 +74,74 @@ export const FeedbackProvider = ({ children }) => {
         editFeedback,
         updateFeedback,
       }}
-    >
-      {children}
+    >{children}
     </FeedbackContext.Provider>
-  );
+  )
 };
 
-export default FeedbackContext;
+export default FeedbackContext
+
+/**WORKING CODE FOR DEPLOYMENT,
+ * FETCHING FROM FeedbackData rather than json-server */
+
+
+// import { createContext, useState } from "react";
+// import feedbackData from "../data/FeedbackData.js";
+
+// const FeedbackContext = createContext();
+
+// const defaultEditState = {
+//   item: {},
+//   edit: false,
+// };
+
+// export const FeedbackProvider = ({ children }) => {
+//   const [feedback, setFeedback] = useState(feedbackData);
+//   const [feedbackEdit, setFeedbackEdit] = useState(defaultEditState);
+
+//   // Add feedback
+//   const addFeedback = (newFeedback) => {
+//     setFeedback([newFeedback, ...feedback]);
+//   };
+
+//   // Delete feedback
+//   const deleteFeedback = (id) => {
+//     if (window.confirm("Are you sure you want to delete?")) {
+//       setFeedback(feedback.filter((item) => item.id !== id));
+//     }
+//   };
+
+//   // Update feedback item
+//   const updateFeedback = (id, updItem) => {
+//     setFeedback(
+//       feedback.map((item) => (item.id === id ? { ...updItem, id } : item))
+//     );
+
+//     setFeedbackEdit(defaultEditState);
+//   };
+
+//   // Set item to be updated
+//   const editFeedback = (item) => {
+//     setFeedbackEdit({
+//       item,
+//       edit: true,
+//     });
+//   };
+
+//   return (
+//     <FeedbackContext.Provider
+//       value={{
+//         feedback,
+//         feedbackEdit,
+//         deleteFeedback,
+//         addFeedback,
+//         editFeedback,
+//         updateFeedback,
+//       }}
+//     >
+//       {children}
+//     </FeedbackContext.Provider>
+//   );
+// };
+
+// export default FeedbackContext;
